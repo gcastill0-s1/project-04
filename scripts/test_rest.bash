53394c64b9d055267f5263d8285fcfdb93018a80
gcastill0-s1 <179959151+gcastill0-s1@users.noreply.github.com>
2024-09-30 12:08:20 -0400

    Updating url for US and Canada

--- scripts/test_rest.bash
+++ scripts/test_rest.bash
@@ -4,8 +4,11 @@ read -s -p "Enter your AI SIEM KEY: " SDL_API_TOKEN
 export SDL_API_TOKEN
 
 # To prevent pollution, we use a base64 blob to store the configuration
-# Can: https://ingest.ca1.sentinelone.net/services/collider/raw?sourcetype=marketplace-paloaltonetworksfirewall-latest
-url_base64="aHR0cHM6Ly9pbmdlc3QuY2ExLnNlbnRpbmVsb25lLm5ldC9zZXJ2aWNlcy9jb2xsaWRlci9yYXc/c291cmNldHlwZT1tYXJrZXRwbGFjZS1wYWxvYWx0b25ldHdvcmtzZmlyZXdhbGwtbGF0ZXN0Cg=="
+# Ca: https://ingest.ca1.sentinelone.net/services/collector/raw?sourcetype=marketplace-paloaltonetworksfirewall-latest
+# url_base64="aHR0cHM6Ly9pbmdlc3QuY2ExLnNlbnRpbmVsb25lLm5ldC9zZXJ2aWNlcy9jb2xsZWN0b3IvcmF3P3NvdXJjZXR5cGU9bWFya2V0cGxhY2UtcGFsb2FsdG9uZXR3b3Jrc2ZpcmV3YWxsLWxhdGVzdAo="
+
+# US: https://ingest.us1.sentinelone.net/services/collector/raw?sourcetype=marketplace-paloaltonetworksfirewall-latest
+url_base64="aHR0cHM6Ly9pbmdlc3QudXMxLnNlbnRpbmVsb25lLm5ldC9zZXJ2aWNlcy9jb2xsZWN0b3IvcmF3P3NvdXJjZXR5cGU9bWFya2V0cGxhY2UtcGFsb2FsdG9uZXR3b3Jrc2ZpcmV3YWxsLWxhdGVzdAo="
 
 message="$(date '+%b %d %H:%M:%S %Z') $(hostname) tester[$$]: Curl message to SDL intake API"
 
@@ -15,7 +18,6 @@ if [ -n "$SDL_API_TOKEN" ]; then
     curl -v -k "$(echo ${url_base64} | base64 -d)" \
     -H "Authorization: Bearer ${SDL_API_TOKEN}" \
     -H "Accept: application/text" \
-    -d '{"message": "${message}"}'
-
+    -d "${message}"
 fi
 
