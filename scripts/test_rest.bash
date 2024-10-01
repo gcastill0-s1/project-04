8677e5499e33df616a39b7910d3e412558930ff8
gcastill0-s1 <179959151+gcastill0-s1@users.noreply.github.com>
2024-10-01 15:30:21 -0400

    Updating script

--- scripts/test_rest.bash
+++ scripts/test_rest.bash
@@ -1,25 +1,30 @@
 #!/bin/bash
 
+# This script is designed to send a test message to the SentinelOne Data Lake (SDL) intake API.
+# It prompts the user to enter their AI SIEM key, sets the necessary environment variables, 
+# and constructs the appropriate API endpoint URL based on the specified region and source type.
+# The script then sends a test message using the curl command to verify the connection and 
+# configuration. The message includes a timestamp, hostname, and process ID for identification.
+
 clear && echo
 
 read -s -p "Enter your AI SIEM KEY: " SDL_API_TOKEN
 export SDL_API_TOKEN
 
-echo && echo
-
-
-# The REGION to the SDL intake API. Region choices are: us1, au1, euw31, ca1, ap1, aps1, and aps2.
-# US: https://ingest.us1.sentinelone.net/services/collector/raw
-# EU: https://ingest.euw31.sentinelone.net/services/collector/raw
-# Ca: https://ingest.ca1.sentinelone.net/services/collector/raw
-# AP: https://ingest.ap1.sentinelone.net/services/collector/raw
-# AP: https://ingest.aps1.sentinelone.net/services/collector/raw
-# AP: https://ingest.aps2.sentinelone.net/services/collector/raw
+# The REGION to the SDL intake API. The default value is us1. The other options are:
+# US1: United States
+# EU1: Europe (AWS)
+# EUW31: Europe (GCP)
+# CA1: Canada
+# AP1: Asia Pacific (Singapore)
+# APS1: Asia Pacific (Mumbai)
+# APS2: Asia Pacific (Sydney)
 
 REGION="us1"
 
-# The default value is paloaltonetworksfirewall. The other options are: fortinetfortimanager, 
-# zscalerinternetaccess, ciscofirewallthreatdefense, and fortinetfortigate
+# The default source is paloaltonetworksfirewall. The other options are: fortinetfortimanager, 
+# zscalerinternetaccess, ciscofirewallthreatdefense, and fortinetfortigate. Alternatively, you
+# can replace the SOURCETYPE variable with syslog.
 SOURCE="paloaltonetworksfirewall"
 
 # The SOURCETYPE to the SDL intake API. 
@@ -27,6 +32,7 @@ SOURCETYPE="marketplace-${SOURCE}-latest"
 
 SDL_URL="https://ingest.${REGION}.sentinelone.net/services/collector/raw?sourcetype=${SOURCETYPE}"
 
+echo && echo
 message="$(date '+%b %d %H:%M:%S %Z') $(hostname) tester[$$]: Test message to SDL intake API using curl and ${SOURCE}"
 echo $message && echo && echo
 
@@ -37,5 +43,5 @@ if [ -n "$SDL_API_TOKEN" ]; then
     -H "Authorization: Bearer ${SDL_API_TOKEN}" \
     -H "Accept: application/text" \
     -d "${message}"
-    
+
 fi
